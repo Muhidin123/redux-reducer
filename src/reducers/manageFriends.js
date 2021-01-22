4773b35a599635abde63bb14d3add621fbdf1f98
Muhidin <muhidinhukic1@gmail.com>
2021-01-21 21:39:30 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,16 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { ...state, friends: [...state.friends, action.friend] };
+    case "REMOVE_FRIEND":
+      const newList = state.friends.filter((friend) => {
+        return friend.id !== action.id;
+      });
+      return {
+        ...state,
+        friends: newList,
+      };
+    default:
+      return state;
+  }
 }
